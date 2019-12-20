1241c633e7cf5af07c09360d742f858af0694527
bangquan.qian <chainz@foxmail.com>
2019-12-20 17:45:23 +0800

    Create Problem0018.java

---
+++ src/main/java/cn/tenbit/algorithms/leetcode/Problem0018.java
@@ -0,0 +1,35 @@
+package cn.tenbit.algorithms.leetcode;
+
+import cn.tenbit.algorithms.util.HawUtils;
+import cn.tenbit.haw.core.support.HawExecutor;
+
+/**
+ * @author bangquan.qian
+ * @date 2019-11-14 19:16
+ */
+public class Problem0018 {
+
+    public static void main(String[] args) {
+        HawUtils.invokeWithStatistics(new HawExecutor() {
+            @Override
+            public Object execute() throws Throwable {
+                test();
+                return null;
+            }
+        });
+    }
+
+    private static void test() {
+
+    }
+
+    /**
+     *
+     */
+    private static class Solution1 {
+
+        /**
+         *
+         */
+    }
+}
